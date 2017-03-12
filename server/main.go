654dfdafba9529747e72af546ca268bfd73101b7
梁惠涌 <lianghuiyong@foxmail.com>
2017-03-13 03:22:35 +0800

    up

---
+++ server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
